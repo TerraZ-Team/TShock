98022aca09d51a8f9151ce7f4b8c923c253e24df
Xekep <Xekep@users.noreply.github.com>
2026-08-24 00:54:43 +0300

    fix: add missing socket IO import

--- TShockAPI/Sockets/LinuxTcpSocket.cs
+++ TShockAPI/Sockets/LinuxTcpSocket.cs
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
